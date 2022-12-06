7e8dee110e42baa1368748f20e621dfe72ef2ae3
yangjing.28 <yangjing.28@bytedance.com>
2022-12-06 09:52:33 +0800

    链表

--- 代码随想录/链表/203. 移除链表元素.go
+++ 代码随想录/链表/203. 移除链表元素.go
@@ -1,1 +1,26 @@
 package 链表
+
+/*
+删除节点：
+判断当前节点的下一个节点是否为待删除节点，如果是让节点next指针直接指向下下一个节点
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeElements(head *ListNode, val int) *ListNode {
+	dummyHead := &ListNode{}
+	dummyHead.Next = head
+	cur := dummyHead
+	for cur != nil && cur.Next != nil {
+		if cur.Next.Val == val { // cur.Next指向的节点为待删除节点
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next // cur.Next指向的节点非待删除节点
+		}
+
+	}
+	return dummyHead.Next
+}
